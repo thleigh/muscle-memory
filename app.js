3d0ae9233639c0cf8129e33aac7f53e6d6c6cce0
Tanner Leigh <tannerhleigh@gmail.com>
2020-07-17 14:08:30 -0700

    [javascript]js variables

---
+++ app.js
@@ -0,0 +1,14 @@
+// 5 variables of different types
+// string, number, array, boolean, object
+//object : (4 different key/value pairs) [string], [number], [array], [boolean]
+
+let stringOne = "This is a string";
+let numberOne = 1;
+let arrayOne = ["one", "two", "three"];
+let booleanOne = true;
+let objectOne = {
+    name: "Tanner",
+    age: 23,
+    hobbies: ["photography", "listening to music", "watching movies", "video games", "rock climbing"],
+    fullStackDeveloper: true
+};
